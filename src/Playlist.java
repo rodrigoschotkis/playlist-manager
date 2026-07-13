fbb0bf4dd6ff180b09de819ddbeab4a2096366bd
rodrigoschotkis <rodrigoschotkis@gmail.com>
2026-07-12 21:47:20 -0300

    Playlist manager class finished

--- src/Playlist.java
+++ src/Playlist.java
@@ -37,6 +37,4 @@ public class Playlist {
         }
         return duration;
     }
-
-
 }
--- src/PlaylistManager.java
+++ src/PlaylistManager.java
@@ -1,2 +1,32 @@
+import java.util.ArrayList;
+
 public class PlaylistManager {
+    private ArrayList<Playlist> playlists = new ArrayList<>();
+
+    public void createPlaylist(Playlist playlist) {
+        playlists.add(playlist);
+    }
+
+    public boolean removePlaylist(String name) {
+        for (Playlist p : playlists) {
+            if (p.getName().equalsIgnoreCase(name)) {
+                playlists.remove(p);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Playlist searchPlaylist(String name) {
+        for (Playlist p : playlists) {
+            if (p.getName().equalsIgnoreCase(name)) {
+                return p;
+            }
+        }
+        return null;
+    }
+
+    public ArrayList<Playlist> getPlaylists() {
+        return new ArrayList<>(playlists);
+    }
 }
